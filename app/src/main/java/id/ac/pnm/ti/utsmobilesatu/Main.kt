1e1df1fa8413787b35bdb86ff56a1a103a2dbef7
Amro Alfien <55046247+alfienrian@users.noreply.github.com>
2024-03-23 22:34:44 +0700

    [fix] - tugas UTS

--- app/src/main/java/id/ac/pnm/ti/utsmobilesatu/Main.kt
+++ app/src/main/java/id/ac/pnm/ti/utsmobilesatu/Main.kt
@@ -4,10 +4,9 @@ fun main() {
     // Inisialisasi mutableList dan tambah data List
     val clubs = mutableListOf(Club("Liverpool", 19, 8, 9, 6, 3))
     clubs.add(Club("Manchester United", 20, 12, 6, 3, 1))
-    clubs.add(Club("Liverpool", 6, 6, 5, 2, 2))
+    clubs.add(Club("Chelsea", 6, 6, 5, 2, 2))
     clubs.add(Club("Manchester City", 8, 8, 8, 0, 0))
     clubs.add(Club("Arsenal", 13, 14, 2, 0, 0))
-
 
     // Passing a named function ::sortByNumberTrophyTotal
     val sortedClubs = filterAndSort(clubs, ::sortByNumberTrophyTotal)
@@ -34,6 +33,10 @@ fun main() {
     println("          Recap hasil perolehan tropi Club           ")
     println("=====================================================")
     println(clubs[0].recap())
+    println(clubs[1].recap())
+    println(clubs[2].recap())
+    println(clubs[3].recap())
+    println(clubs[4].recap())
 }
 
 // High order function filter and sort
@@ -47,7 +50,6 @@ fun sortByNumberTrophyTotal(clubs: List<Club>): List<Club> {
         it.totalTrophy
     }
     return sortedClubs
-
 }
 
 // Fungsi filter
